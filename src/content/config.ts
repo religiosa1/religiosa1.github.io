6cc61964c4fd1d8fb285a8a33c389b481bef6832
Viacheslav Azarov <public@religiosa.ru>
2023-02-15 12:25:47 +0100

    Formatting changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,28 +3,28 @@ import { ingredientsSchema } from "~/models/Ingredient";
 import { articleSchema } from '~/models/Article';
 
 const recipeCollection = defineCollection({
-  schema: z.object({
-    title: z.string().min(1),
-    originalTitle: z.string().optional(),
-    recommended: z.array(z.string()).optional(),
-    time: z.string(), // TODO regex
-    waiting: z.string().optional(), //TODO regex
-    ingredients: z.union([
-      z.record(z.array(ingredientsSchema)),
-      z.array(ingredientsSchema),
-    ]),
-    sources: z.array(z.string()).optional(),
-  }),
+	schema: z.object({
+		title: z.string().min(1),
+		originalTitle: z.string().optional(),
+		recommended: z.array(z.string()).optional(),
+		time: z.string(), // TODO regex
+		waiting: z.string().optional(), //TODO regex
+		ingredients: z.union([
+			z.record(z.array(ingredientsSchema)),
+			z.array(ingredientsSchema),
+		]),
+		sources: z.array(z.string()).optional(),
+	}),
 });
 
 const articlesCollection = defineCollection({
-  schema: articleSchema
+	schema: articleSchema
 });
 
 export const collections = {
-  'recepies': recipeCollection,
-  'electronics': articlesCollection,
-  'polski': articlesCollection,
-  'programming': articlesCollection,
-  'woodworking': articlesCollection,
+	'recepies': recipeCollection,
+	'electronics': articlesCollection,
+	'polski': articlesCollection,
+	'programming': articlesCollection,
+	'woodworking': articlesCollection,
 };
--- src/models/Ingredient.ts
+++ src/models/Ingredient.ts
@@ -1,14 +1,14 @@
 import { z } from "astro:content";
 
 export const ingredientsSchema = z.object({
-  /** Название */
-  name: z.string(),
-  /** Количество с ед. измерения */
-  desc: z.string().optional(),
-  /** Альтернативная единица измерения */
-  quant: z.string().optional(),
-  /** Описание или пояснение */
-  alt: z.string().optional(),
+	/** Название */
+	name: z.string(),
+	/** Количество с ед. измерения */
+	desc: z.string().optional(),
+	/** Альтернативная единица измерения */
+	quant: z.string().optional(),
+	/** Описание или пояснение */
+	alt: z.string().optional(),
 });
 
 export type Ingredient = z.infer<typeof ingredientsSchema>;
